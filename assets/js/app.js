34d2a343b8e75a2654d10771b3c09130e468edce
Yuliy Sozin <j.batkovich@gmail.com>
2020-05-28 18:41:04 -0700

    Theme toggle now saves to LS. It works!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,32 @@
-const theme_toggler = document.querySelector('#theme-toggler');
+// theme toggling
+const theme_toggler = document.querySelector("#theme-toggler");
 
-theme_toggler.addEventListener('change', e => {
-  if (e.target.checked) {
-    document.body.classList.toggle('light');
-  } else {
-    document.body.classList.toggle('light');
-  }
+theme_toggler.addEventListener("change", function (e) {
+	updateTheme(); // update color theme
 });
+
+const initTheme = () => {
+	let lightThemeSelected =
+		localStorage.getItem("themeSwitch") !== null &&
+		localStorage.getItem("themeSwitch") === "light";
+
+	//   update checkbox
+	theme_toggler.checked = lightThemeSelected;
+	// update class
+	lightThemeSelected
+		? document.body.classList.add("light")
+		: document.body.classList.remove("light");
+};
+
+const updateTheme = () => {
+	if (theme_toggler.checked) {
+		// light theme selected
+		document.body.classList.add("light");
+		localStorage.setItem("themeSwitch", "light"); // save theme selection
+	} else {
+		document.body.classList.remove("light");
+		localStorage.removeItem("themeSwitch"); // reset theme selection
+	}
+};
+
+initTheme();
